2de8b75498aa7f3005f4ec0b817801595b1f2e72
BenAhmedAmir <dragotn@gmail.com>
2021-01-17 21:31:03 +0100

    front crud employee

---
+++ src/app/interceptor.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpInterceptor, HttpRequest,
+  HttpHandler, HttpEvent, HttpErrorResponse
+} from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+@Injectable({
+  providedIn: 'root'
+})
+export class InterceptorService implements HttpInterceptor {
+  intercept(req: HttpRequest<any>, next: HttpHandler) {
+    const xhr = req.clone({
+      headers: req.headers.set('X-Requested-With', 'XMLHttpRequest')
+    });
+    return next.handle(xhr);
+  }
+}
